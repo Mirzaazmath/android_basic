078672ef23067cc961afd7d2c54a72b09459fc97
Mirzaazmath <azmath9603399270@gmail.com>
2024-11-29 12:03:05 +0530

    alert dialog

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,13 +10,16 @@ import android.widget.Toast;
 
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-    // Creating the Variable to Access the value from UI
-    private CheckBox burger, pizza, chips, drinks,selectAll;
-    private Button btn;
+    // Creating Variable for Accessing the element from  UI
+    Button closeButton;
+    // here we are creating AlertDialog.Builder to build the alert dialog
+    AlertDialog.Builder builder;
+
 
 
     @Override
@@ -23,58 +27,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // Call a Method to all logic
-        addListenerToButton();
-    }
+        // Assigning the value to our Variable  using Id
+        closeButton =findViewById(R.id.CloseBtn);
+        // Here we are instance of Alert dialog
+        builder=new AlertDialog.Builder(this);
+       // Here we are Add Listener to Our Close Button
+        closeButton.setOnClickListener((v)->{
+            // here we are setting our message into out alert dialog
+            builder.setMessage("Do you want to Close the App ?")
+                    // Here we are setting weather our alert dialog is cancelable or not
+                    .setCancelable(false).
+                    // Here we are adding yes button to our alert dialog and also adding the listener to perform some tasks
+                    setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    // if we use   finish(); it will close the entire application
+                   // finish();
+                    // here we are closing the alert dialog
+                    dialog.cancel();
+                    // here we are displaying the toast message
+                    Toast.makeText(getApplicationContext(),"You have Pressed Yes", Toast.LENGTH_LONG).show();
 
-    public void addListenerToButton() {
-        burger = findViewById(R.id.cbB);
-        pizza = findViewById(R.id.cbP);
-        chips = findViewById(R.id.cbC);
-        drinks = findViewById(R.id.cbD);
-        selectAll=findViewById(R.id.cbA);
-        btn =findViewById(R.id.btn1);
-
-        selectAll.setOnCheckedChangeListener((buttonView, isChecked) -> {
-            burger.setChecked(isChecked);
-            pizza.setChecked(isChecked);
-            chips.setChecked(isChecked);
-            drinks.setChecked(isChecked);
+                }
+                        // Here we are adding yes button to our alert dialog and also adding the listener to perform some tasks
+            }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    // here we are closing the alert dialog
+                    dialog.cancel();
+                    // here we are displaying the toast message
+                    Toast.makeText(getApplicationContext(),"You have Pressed No", Toast.LENGTH_LONG).show();
+                }
+                        // Here we are adding Title of our alert dialog
+            }).setTitle("Android Alert Example");
+           // here we are creating the alert dialog
+            AlertDialog alert = builder.create();
+            // here we are showing the alert dialog
+            alert.show();
 
         });
-
-
-
-        btn.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View view) {
-                int totalamount=0;
-                StringBuilder result=new StringBuilder();
-                result.append("Selected Items:");
-                if(burger.isChecked()){
-                  //  result.append("\nBurger 120Rs");
-                    totalamount+=120;
-                }
-                if(pizza.isChecked()){
-                    //result.append("\nPizza 100Rs");
-                    totalamount+=100;
-                }
-                if(chips.isChecked()){
-                   // result.append("\nChips 50Rs");
-                    totalamount+=50;
-                }
-                if(drinks.isChecked()){
-                   // result.append("\nDrinks 60Rs");
-                    totalamount+=50;
-                }
-
-                result.append("\nTotal: "+totalamount+"Rs");
-                //Displaying the message on the toast
-                Toast.makeText(getApplicationContext(), result.toString(), Toast.LENGTH_LONG).show();
-            }
-        });
-
 
     }
 
