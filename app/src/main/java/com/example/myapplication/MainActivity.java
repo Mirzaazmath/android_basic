2bc0d63a46af77053ad5b2a594089e65983bb8ae
Mirzaazmath <azmath9603399270@gmail.com>
2024-11-29 15:25:16 +0530

    added ListView

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,21 +1,24 @@
 package com.example.myapplication;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
-// To use the Dropdown Menu we need to implements our class with  AdapterView.OnItemSelectedListener
-public class MainActivity extends AppCompatActivity  implements AdapterView.OnItemSelectedListener {
-// Here we are Creating the list of String
-String[] country={"India","Spain","Russia","USA"};
-String selectedCountry = "";
+import org.w3c.dom.Text;
 
 
+public class MainActivity extends AppCompatActivity {
+    ListView listView;
+
+    String[] listItem;
 
 
     @Override
@@ -23,28 +26,24 @@ String selectedCountry = "";
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        //Getting the instance of Spinner and applying OnItemSelectedListener on it
-        Spinner spin =  findViewById(R.id.spiner);
-        spin.setOnItemSelectedListener(this);
-       //Creating the ArrayAdapter instance having the country list
-        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,country);
-        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        listView=findViewById(R.id.listView);
+        listItem = getResources().getStringArray(R.array.array_technology);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, listItem);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                // TODO Auto-generated method stub
+                String value=adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
 
-        //Setting the ArrayAdapter data on the Spinner
-        spin.setAdapter(aa);
+            }
+        });
+
 
 
     }
-    //Performing action onItemSelected and onNothing selected
 
-    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
-        selectedCountry=country[position];
-        Toast.makeText(getApplicationContext(),country[position] , Toast.LENGTH_LONG).show();
-        System.out.println(selectedCountry);
-    }
-    @Override
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
 
 }
