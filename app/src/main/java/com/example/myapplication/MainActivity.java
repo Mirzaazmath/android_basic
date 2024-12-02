a522af21f0db8de833424fde8bb37747acbc0062
Mirzaazmath <azmath9603399270@gmail.com>
2024-12-02 16:17:55 +0530

    added both clock

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,24 +1,11 @@
 package com.example.myapplication;
 import android.os.Bundle;
-import android.widget.Button;
-
-import android.widget.TextView;
-import android.widget.TimePicker;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
-
 public class MainActivity extends AppCompatActivity {
-    // Created Variable
-    TimePicker timePicker;
-    TextView textView;
-     Button button;
-
-
-
 
 
     @Override
@@ -26,30 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        // Assigning value to variable based on id
-        timePicker = findViewById(R.id.time1);
-        textView= findViewById(R.id.txt1);
-        button = findViewById(R.id.btn1);
-      // Adding Listener
-        button.setOnClickListener((v)->{
-            textView.setText("Selected Time : "+ getTime());
-        });
 
     }
-
-    public String getTime(){
-        StringBuilder builder =new StringBuilder();
-        // Getting Hour
-        builder.append(timePicker.getHour()).append(":");
-        // Getting Minute
-        builder.append(timePicker.getMinute());
-        // Returning String
-        return builder.toString();
-
-    }
-
-
-
-
 
 }
