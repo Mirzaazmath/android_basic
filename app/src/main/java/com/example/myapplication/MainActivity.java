ff9ce93eff93813eced55ebe365fe13cd1302ee5
Mirzaazmath <azmath9603399270@gmail.com>
2024-12-04 12:41:07 +0530

    added actitvity life cycle

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,28 +1,15 @@
 package com.example.myapplication;
-import android.app.ActionBar;
 import android.os.Bundle;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.widget.Button;
-import android.widget.ImageSwitcher;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.ViewSwitcher;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
 
 
 public class MainActivity extends AppCompatActivity {
-SeekBar seekBar;
-ImageSwitcher imageSwitcher;
 
-int[] imageSwitcherImages ={R.drawable.youtube,R.drawable.twitter,R.drawable.insta,R.drawable.google,R.drawable.drive};
 
-int imageSwitcherLenght=imageSwitcherImages.length;
-int counter =-1;
+
 
 
 
@@ -32,62 +19,41 @@ int counter =-1;
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        imageSwitcher = findViewById(R.id.imageSwitcher);
-        seekBar = findViewById(R.id.seekBar);
-
-        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
-            @Override
-            public View makeView() {
-                ImageView switcherImage=new ImageView(getApplicationContext());
-                switcherImage.setLayoutParams(new ImageSwitcher.LayoutParams(
-                        ActionBar.LayoutParams.FILL_PARENT, ActionBar.LayoutParams.FILL_PARENT
-                ));
-                switcherImage.setScaleType(ImageView.ScaleType.FIT_CENTER);
-                switcherImage.setImageResource(R.drawable.youtube);
-                //switcherImageView.setMaxHeight(100);
-                return switcherImage;
-            }
-        });
-        Animation aniOut =  AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
-        Animation aniIN =  AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
-
-        imageSwitcher.setOutAnimation(aniOut);
-        imageSwitcher.setInAnimation(aniIN);
-        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-
-                if (progress<20){
-                    imageSwitcher.setImageResource(imageSwitcherImages[0]);
-                } else  if (progress<40 && progress>20){
-                    imageSwitcher.setImageResource(imageSwitcherImages[1]);
-                }else  if (progress<60 && progress>40){
-                    imageSwitcher.setImageResource(imageSwitcherImages[2]);
-                }else  if (progress<80 && progress>60){
-                    imageSwitcher.setImageResource(imageSwitcherImages[3]);
-                }else  if (progress<100 && progress>80){
-                    imageSwitcher.setImageResource(imageSwitcherImages[4]);
-                }
-
-
-
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                // Toast.makeText(MainActivity.this, "Progress Starts", Toast.LENGTH_SHORT).show();
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                // Toast.makeText(MainActivity.this, "Progress Ends", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
+        Log.d("lifecycle","onCreate invoked");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("lifecycle","onStart invoked");
 
     }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("lifecycle","onResume invoked");
     }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("lifecycle","onPause invoked");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("lifecycle","onStop invoked");
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("lifecycle","onRestart invoked");
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("lifecycle","onDestroy invoked");
+    }
+}
 
 
 
