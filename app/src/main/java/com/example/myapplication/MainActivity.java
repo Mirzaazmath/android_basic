b83f9ec445c2d38f88b45b0f0014d88ecbfae8db
Mirzaazmath <azmath9603399270@gmail.com>
2024-12-18 12:17:43 +0530

    added call feature

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,47 +1,51 @@
 package com.example.myapplication;
-import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
-
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import android.Manifest;
 
 
 public class MainActivity extends AppCompatActivity {
-    Button startBtn,stopBtn,nextBtn;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        nextBtn = findViewById(R.id.next);
-        startBtn = findViewById(R.id.start);
-        stopBtn = findViewById(R.id.stop);
-        // Here we are Creating the navigatorIntent
-        Intent navigatorIntent=new Intent(getApplicationContext(), SecondActivity.class);
-        nextBtn.setOnClickListener((v)-> {
-            startActivity(navigatorIntent);
-        });
 
-        startBtn.setOnClickListener((v)-> {
-            // Here we are Starting the service that we have created
-           startService(new Intent(this,MyService.class));
-        });
-        stopBtn.setOnClickListener((v)-> {
-             // Here we are Stopping the service that we have created
-            stopService(new Intent(this, MyService.class));
-        });
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},1);
+        }
+        Button callButton = findViewById(R.id.callBtn);
 
+        callButton.setOnClickListener((v -> {
+
+            Intent callIntent= new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:9603399270"));
+
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},1);
+            }
+            else
+            {
+                startActivity(callIntent);
+            }
+
+        }));
 
 
     }
 
+
+
 }
+
+
 
 
 
